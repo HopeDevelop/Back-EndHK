627d32c931d67e94e2d14209a8589f8a357a2159
Bruno Mourao <brunolmourao10@gmail.com>
2017-06-17 14:54:40 -0300

    Classes do model e JSON

---
+++ Hope Keeper/src/model/Donation.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+public class Donation {
+	private String description;
+	private String type;
+	private ArrayList<String> flags;
+	private String title;
+	public Donation(String title,String description,String type){
+		this.setTitle(title);
+		this.setDescription(description);
+		this.setType(type);
+		this.flags = new ArrayList<String>();
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public ArrayList<String> getFlags() {
+		return flags;
+	}
+	public void setFlags(ArrayList<String> flags) {
+		this.flags = flags;
+	}
+	public JSONObject createJSON(){
+		JSONObject json = new JSONObject();
+		json.put("title",this.getTitle());
+		json.put("description",this.getDescription());
+		json.put("type", this.getType());
+		json.put("flags", this.getFlags());
+		return json;
+	}
+}
