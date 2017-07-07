70f814dfa0a03fca641cdee24ba63d33bf45508a
Mateus Corrêa <mcorrea.comp@gmail.com>
2017-07-06 22:21:04 -0300

    data.sql added

---
+++ data.sql
@@ -0,0 +1,36 @@
+INSERT INTO admins VALUES
+('Admin', '12345');
+
+INSERT INTO receptor_requests VALUES
+(
+	'username1',
+	'password1',
+	'name1',
+	'cnpj1',
+	'address1',
+	'email1',
+	'site1',
+	'description1'
+),
+
+(
+	'username2',
+	'password2',
+	'name2',
+	'cnpj2',
+	'address2',
+	'email2',
+	'site2',
+	'description2'
+),
+
+(
+	'username3',
+	'password3',
+	'name3',
+	'cnpj3',
+	'address3',
+	'email3',
+	'site3',
+	'description3'
+);
